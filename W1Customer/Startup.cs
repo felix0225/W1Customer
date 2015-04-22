bcb00b55d5df4a9d8a65325ed1018fb4c236c5b5
felix chen <kib37704@outlook.com>
2015-04-22 21:30:25 +0800

    新增專案

---
+++ W1Customer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(W1Customer.Startup))]
+namespace W1Customer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
